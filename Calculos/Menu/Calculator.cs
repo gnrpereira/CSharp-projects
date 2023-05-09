f213c23ab54a237670bcd221f1c100940b805ac3
gnrpereira <gabrielgv7191@outlook.com>
2023-05-09 10:52:16 -0300

    Atualização arquivos

--- Calculos/Menu/Calculator.cs
+++ Calculos/Menu/Calculator.cs
@@ -39,7 +39,7 @@ namespace CalculadoraSimples
             decimal num2 = decimal.Parse(Console.ReadLine());
 
             decimal sum = num1 * num2;
-            Console.WriteLine($"The product of the {num1} and {num2} is: {sum} a");
+            Console.WriteLine($"The product of the {num1} and {num2} is: {sum}");
         }
         public static void Division()
         {
